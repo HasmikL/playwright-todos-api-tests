fd2be352e4855a93330bbd04ad49175e81680846
HasmikL <hasmik.levonyan07@gmail.com>
2026-02-24 15:54:03 +0400

    Add all todos tests including POST and PATCH updates

--- tests/todos.spec.ts
+++ tests/todos.spec.ts
@@ -1,6 +1,34 @@
 
 import { test, expect } from '@playwright/test';
 
+
+  const todoId = 7;
+
+  const newTODO = {
+    "userId": 1,
+    "id": 1,
+    "title": "Do Homework",
+    "completed": true
+  }
+
+  const updatedToDO = {
+    "userId": 5,
+    "id": 1,
+    "title": "Do Your Homework",
+    "completed": true
+  }
+
+  const partialUpdatedToDo = {
+
+    "userId": 5,
+    "id": 1,
+    "title": "Complete the Restful Booker",
+    "completed": true
+
+  }
+
+
+
 test.describe('Todos API Tests', async() => {
    
     test(' Get All Todos',async ({request}) => {
@@ -47,7 +75,6 @@ test.describe('Todos API Tests', async() => {
      
   });
 
-     const todoId = 7;
 
    test(`Get ${todoId}th Todo`, async ({ request }) => {
 
@@ -65,4 +92,85 @@ test.describe('Todos API Tests', async() => {
         expect(todo).toHaveProperty('completed');
 });   
 
+   test('POST- create a new TODO', async({request})=>{
+    const response = await request.post('https://jsonplaceholder.typicode.com/todos',{
+      data:newTODO
+    });
+
+    expect(response.ok()).toBe(true);
+    expect(response.status()).toBe(201);
+
+    const data = await response.json();
+
+    expect(data).toHaveProperty('userId',newTODO.userId);
+    expect(data).toHaveProperty('id');
+    expect(data).toHaveProperty('title',newTODO.title);
+    expect(data).toHaveProperty('completed',newTODO.completed);
+
+    //console.log(newTODO);
+    
+   });
+
+   test(`PUT- update TODO ${todoId}`, async({request})=>{
+    const response = await request.put(`https://jsonplaceholder.typicode.com/todos/${todoId}`,{
+      data:updatedToDO
+    });
+
+    expect(response.ok()).toBe(true);
+    expect(response.status()).toBe(200);
+
+    const data = await response.json();
+
+    expect(data).toHaveProperty('userId',updatedToDO.userId);
+    expect(data).toHaveProperty('id');
+    expect(data).toHaveProperty('title',updatedToDO.title);
+    expect(data).toHaveProperty('completed',updatedToDO.completed);
+
+    //console.log(updatedToDO);
+    
+   });
+
+   test(`PATCH- partially update TODO ${todoId}`, async({request})=>{
+    const response = await request.patch(`https://jsonplaceholder.typicode.com/todos/${todoId}`,{
+      data:partialUpdatedToDo
+    });
+
+    expect(response.ok()).toBe(true);
+    expect(response.status()).toBe(200);
+
+    const data = await response.json();
+
+   
+    expect(data).toHaveProperty('title',partialUpdatedToDo.title);
+    
+
+    //console.log(partialUpdatedToDo);
+    
+   });
+
+   test(`DELETE- TODO ${todoId}`, async({request})=>{
+    const response = await request.delete(`https://jsonplaceholder.typicode.com/todos/${todoId}`,{
+      
+    });
+
+    expect(response.ok()).toBe(true);
+    expect(response.status()).toBe(200);
+
+    const data = await response.json();
+
+    expect(data).toEqual({});
+    
+    
+   });
+
+   test('GET Negative Test', async({request})=>{
+      const resposne = await request.get('https://jsonplaceholder.typicode.com/todos/999999999999999');
+
+      expect(resposne.ok()).toBe(false);
+      expect(resposne.status()).toBe(404);
+
+    });
+
+
 });
+
